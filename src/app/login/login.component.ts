b59872d5ce2a5ffe2c4c29d9c15217a957d7ef5e
SubikshaRamkumar <114942854+SubikshaRamkumar@users.noreply.github.com>
2024-06-25 16:51:21 +0530

    login form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  formdata = { email: '', password: '' };
+  submit = false;
+  loading = false;
+  errorMessage = '';
+  onSubmit() {
+    this.loading = true;
+    console.log(this.formdata);
+  }
+}
