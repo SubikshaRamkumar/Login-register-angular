cf4946d5cf35201bce86fd80ae7299cb04974c07
SubikshaRamkumar <114942854+SubikshaRamkumar@users.noreply.github.com>
2024-06-25 10:28:16 +0530

    validation for all fields and loading icon and button disabled

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FormsModule, CommonModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  submit = false;
+  formdata = { name: '', email: '', password: '' };
+  errorMessage = '';
+  loading = false;
+  onSubmit() {
+    console.log(this.formdata);
+    this.loading = true;
+  }
+}
